cb9fe366cd6c48d82f136444e132d1ea7713aab8
sacsabado <nicolasbareiro07@gmail.com>
2023-10-11 09:07:10 -0300

    codigo2

---
+++ index/reconocer2.js
@@ -0,0 +1,36 @@
+const textoReconocidoElement = document.querySelector("p")
+const boton = document.querySelector("button")
+
+if ("webkitSpeechRecognition" in window) {
+    const reconocimiento = new webkitSpeechRecognition();
+
+    reconocimiento.continuos = true;
+    reconocimiento.lang = "es-Es"; 
+
+    reconocimiento.onstart = function () {
+        textReconocidoElement.textContent = "Escuchando...";
+
+    }
+
+    reconocimiento.onresult = function (event) {
+        const resultado = event.results[event.results.length -1]
+        const texto = resultado[0].transcript;
+        textReconocidoElement.textContent = `Texto reconocido: ${texto}`;
+    }
+    reconocimiento.onend = function () {
+        let miTexto = textoReconocidoElement.textContent
+        textoReconocidoElement.textContent += ' (Fin del reconocimiento)';
+        repetirTexto(miTexto)
+
+    };
+
+    boton.addEventListener("click", function () {
+
+        reconocimiento.start();
+
+    });
+
+}else {             
+    textoReconocidoElement.textContent = 'El reconocimiento de voz no es compatible con tu navegador.';
+    boton.disabled = true;
+}
